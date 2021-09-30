004cb7010a6aeb936bd9c77991b23fa81fa50dab
RDIPIPOM <ezamora2@ucol.mx>
2021-09-29 20:03:05 -0500

    Add DishCard component

---
+++ src/components/DishCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dish-card">
+    <div class="dish-body">
+      <div>
+        <h4 class="dish-title">{{ dish.name }}</h4>
+        <p class="dish-description">{{ dish.description }}</p>
+      </div>
+      <div>
+        <div class="dish-location-price">
+          <p class="dish-location">{{ dish.location }}</p>
+          <p class="dish-price">{{ dish.price }}</p>
+        </div>
+      </div>
+    </div>
+    <!-- <img class="dish-picture" src="@/assets/img/dish-pictures/chilaquiles.jpg"> -->
+    <img class="dish-picture" v-bind:src='dish.path_photography'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DishCard',
+  props: ['dish']
+}
+</script>
+
+<style lang="scss" src="../scss/components/DishCard/_skeleton.scss"></style>
+<style lang="scss" src="../scss/components/DishCard/_skin.scss"></style>
