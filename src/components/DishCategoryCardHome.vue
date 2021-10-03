f27d947eae54889ecbeff60f01ccc8bc391f14c6
RDIPIPOM <ezamora2@ucol.mx>
2021-10-03 12:27:01 -0500

    Renovate styles in HomeView

--- src/components/DishCategoryCardHome.vue
+++ src/components/DishCategoryCardHome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dish-category-menu">
-    <h5 class="h5 dish-category">{{ dishCategory.name }}</h5>
+    <h4 class="h4 dish-category">{{ dishCategory.name }}</h4>
     <DishCard v-for="dish in dishCategory.dishes" v-bind:key="dish.id" v-bind:dish="dish"></DishCard>
     <div class="btn-view-all">
       <p class="text-view-all">Ver todos</p>
