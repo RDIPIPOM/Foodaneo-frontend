b5add435a9e1c579dfa4f0ef356aa7ec8df54d15
RDIPIPOM <ezamora2@ucol.mx>
2021-10-08 23:50:18 -0500

    Cart style renovation

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -6,15 +6,32 @@
           <img v-on:click="$emit('close-cart')" class="btn-x" src="../assets/img/x-icon.svg">
           <h4 class="h4 text-shopping-cart">Carrito de compras</h4>
         </div>
-        <h6 class="h6 cart-title-articles">Artículos (1)</h6>
-        <div class="cart-article">
-          <div class="cart-article-body">
+        <h6 class="h6 cart-title-articles">Artículos&nbsp;<span class="cart-articles-counter">(2)</span></h6>
+        <div class="card-cart-article">
+          <div class="cart-article-header">
             <h6 class="h6 cart-article-title">Sandwich</h6>
-            <div class="medium-body cart-article-note">Sin jitomate, con poco aderezo</div>
-            <div class="cart-article-price">$64.79</div>
+            <h6 class="h6 cart-article-counter">(1)</h6>
           </div>
-          <img class="cart-article-image">
+          <div class="cart-article-body">
+            <div class="medium-body cart-article-note">Sin jitomate, con poco aderezo.</div>
+            <div class="medium-body cart-article-price">$64.79</div>
+          </div>
         </div>
+        <div class="card-cart-article">
+          <div class="cart-article-header">
+            <h6 class="h6 cart-article-title">Sandwich</h6>
+            <h6 class="h6 cart-article-counter">(1)</h6>
+          </div>
+          <div class="cart-article-body">
+            <div class="medium-body cart-article-note">Sin jitomate, con poco aderezo.</div>
+            <div class="medium-body cart-article-price">$64.79</div>
+          </div>
+        </div>
+        <div class="cart-subtotal-container">
+          <h4 class="h4 cart-subtotal-text">Subtotal:&nbsp;<span class="cart-subtotal">$129.58</span></h4>
+        </div>
+        <div class="button btn-option-cart btn-add-articles">Agregar más productos</div>
+        <div class="button btn-option-cart btn-finish-order">Terminar orden</div>
       </div>
     </div>
   </transition>
