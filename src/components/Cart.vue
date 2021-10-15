b87a978d814cf93845edf7be74e2cff0f075cd6a
RDIPIPOM <ezamora2@ucol.mx>
2021-10-14 22:33:40 -0500

    Solve cart total

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -9,7 +9,7 @@
         <h6 class="h6 cart-title-articles">Artículos&nbsp;<span class="cart-articles-counter">({{order.order_details.length}})</span></h6>
         <CartArticleCard v-for="cartArticle in order.order_details" v-bind:key="cartArticle.order_detail_id" v-bind:cartArticle="cartArticle"></CartArticleCard>
         <div class="cart-subtotal-container">
-          <h4 class="h4 cart-subtotal-text">Subtotal:&nbsp;<span class="cart-subtotal">$129.58</span></h4>
+          <h4 class="h4 cart-subtotal-text">Subtotal:&nbsp;<span class="cart-subtotal">${{order.total}}</span></h4>
         </div>
         <div class="button btn-option-cart btn-add-articles">Agregar más productos</div>
         <div class="button btn-option-cart btn-finish-order">Terminar orden</div>
@@ -36,6 +36,7 @@ export default {
     if (!isNaN(localStorage.user_id)) {
       getCurrentOrder(localStorage.user_id).then((res) => {
         this.order = res.data
+        console.log(this.order)
       })
     } else {
       this.order.order_details = []
